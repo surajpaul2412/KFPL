3b9df05b510888ae8665045dc8f6f30f96f9008e
surajpaul2412 <suraj.paul.69@gmail.com>
2023-11-27 19:03:19 +0530

    27

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,26 +12,25 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-		
+    {		
 		// ROLE SEEDER
-		$arr = [
-		  ['name' => 'Admin', 'description' => 'Administrator'],
-		  ['name' => 'Trader', 'description' => 'Trader Users'],
-		  ['name' => 'Ops', 'description' => 'Users in Operations'],
-		  ['name' => 'Backoffice', 'description' => 'Back Office Users'],
-		  ['name' => 'Dealer', 'description' => 'Dealers'],
-		  ['name' => 'Accounts', 'description' => 'Account Users']
-		];
-		foreach($arr as $row)
-		{
-		  Role::create($row);
-		}
+		if (Role::count() === 0) {
+            // ROLE SEEDER
+            $arr = [
+                ['name' => 'Admin', 'description' => 'Administrator'],
+                ['name' => 'Trader', 'description' => 'Trader Users'],
+                ['name' => 'Ops', 'description' => 'Users in Operations'],
+                ['name' => 'Backoffice', 'description' => 'Back Office Users'],
+                ['name' => 'Dealer', 'description' => 'Dealers'],
+                ['name' => 'Accounts', 'description' => 'Account Users']
+            ];
+
+            foreach ($arr as $row) {
+                Role::create($row);
+            }
+            $this->command->info('Roles table seeded successfully!');
+        } else {
+            $this->command->info('Roles table already has data. No need to seed.');
+        }
     }
 }
