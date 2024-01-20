ebe7dcccdbac89ecd327f554fc1ca699687424d6
Chandan Patra <chandan.patra@gmail.com>
2024-01-20 20:05:14 +0530

    Menus for Users with Multiple Roles

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -36,38 +36,85 @@
                 <li class="nav-item">
                     <a href="" class="nav-link"><i class="ri-article-line"></i> <span>Templates</span></a>
                 </li>
-                @endif
-                @if(auth()->user()->isTrader())
-                <li class="nav-item">
-                    <a href="{{ route('trader.dashboard') }}" class="nav-link {{ Request::is('trader/dashboard*') ? 'active' : '' }}"><i class="ri-home-5-line"></i> <span>Dashboard</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('trader.tickets.index') }}" class="nav-link {{ Request::is('trader/tickets*') ? 'active' : '' }}"><i class="ri-flag-2-line"></i> <span>Tickets</span></a>
-                </li>
-                @endif
-                @if(auth()->user()->isOps())
-                <li class="nav-item">
-                    <a href="{{ route('ops.dashboard') }}" class="nav-link {{ Request::is('ops/dashboard*') ? 'active' : '' }}"><i class="ri-home-5-line"></i> <span>Dashboard</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('ops.tickets.index') }}" class="nav-link {{ Request::is('ops/tickets*') ? 'active' : '' }}"><i class="ri-flag-2-line"></i> <span>Tickets</span></a>
-                </li>
-                @endif
-                @if(auth()->user()->isAccounts())
-                <li class="nav-item">
-                    <a href="{{ route('accounts.dashboard') }}" class="nav-link {{ Request::is('accounts/dashboard*') ? 'active' : '' }}"><i class="ri-home-5-line"></i> <span>Dashboard</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('accounts.tickets.index') }}" class="nav-link {{ Request::is('accounts/tickets*') ? 'active' : '' }}"><i class="ri-flag-2-line"></i> <span>Tickets</span></a>
-                </li>
-                @endif
-                @if(auth()->user()->isDealer())
-                <li class="nav-item">
-                    <a href="{{ route('dealer.dashboard') }}" class="nav-link {{ Request::is('dealer/dashboard*') ? 'active' : '' }}"><i class="ri-home-5-line"></i> <span>Dashboard</span></a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('dealer.tickets.index') }}" class="nav-link {{ Request::is('dealer/tickets*') ? 'active' : '' }}"><i class="ri-flag-2-line"></i> <span>Tickets</span></a>
-                </li>
+                @else
+                    @php
+                      $user_roles =  auth()->user()->roles;
+                      $roles_names = [];
+                      foreach($user_roles as $role)
+                      {
+                        $roles_names[] = strtolower($role->name);
+                      }
+                      $roleCount = count($roles_names);
+                    @endphp
+
+                    @if($roleCount == 1)
+                      @if(auth()->user()->isTrader())
+                      <li class="nav-item">
+                          <a href="{{ route('trader.dashboard') }}" class="nav-link {{ Request::is('trader/dashboard*') ? 'active' : '' }}"><i class="ri-home-5-line"></i> <span>Dashboard</span></a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="{{ route('trader.tickets.index') }}" class="nav-link {{ Request::is('trader/tickets*') ? 'active' : '' }}"><i class="ri-flag-2-line"></i> <span>Tickets</span></a>
+                      </li>
+                      @endif
+                      @if(auth()->user()->isOps())
+                      <li class="nav-item">
+                          <a href="{{ route('ops.dashboard') }}" class="nav-link {{ Request::is('ops/dashboard*') ? 'active' : '' }}"><i class="ri-home-5-line"></i> <span>Dashboard</span></a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="{{ route('ops.tickets.index') }}" class="nav-link {{ Request::is('ops/tickets*') ? 'active' : '' }}"><i class="ri-flag-2-line"></i> <span>Tickets</span></a>
+                      </li>
+                      @endif
+                      @if(auth()->user()->isAccounts())
+                      <li class="nav-item">
+                          <a href="{{ route('accounts.dashboard') }}" class="nav-link {{ Request::is('accounts/dashboard*') ? 'active' : '' }}"><i class="ri-home-5-line"></i> <span>Dashboard</span></a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="{{ route('accounts.tickets.index') }}" class="nav-link {{ Request::is('accounts/tickets*') ? 'active' : '' }}"><i class="ri-flag-2-line"></i> <span>Tickets</span></a>
+                      </li>
+                      @endif
+                      @if(auth()->user()->isDealer())
+                      <li class="nav-item">
+                          <a href="{{ route('dealer.dashboard') }}" class="nav-link {{ Request::is('dealer/dashboard*') ? 'active' : '' }}"><i class="ri-home-5-line"></i> <span>Dashboard</span></a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="{{ route('dealer.tickets.index') }}" class="nav-link {{ Request::is('dealer/tickets*') ? 'active' : '' }}"><i class="ri-flag-2-line"></i> <span>Tickets</span></a>
+                      </li>
+                      @endif
+                    @endif
+
+
+                    <!-- for Multiple ROLES :: STARTS -->
+
+                      @php
+                      if($roleCount > 1)
+                      {
+                         // FIND USER ROLES attached to URL
+                         $url = str_replace(URL('/'), '', URL::current());
+                         $url = trim(rtrim(ltrim(trim($url),'/'),'/'));
+                         $parts = explode('/', $url);
+
+                         // SELECTED ROLE
+                         $current_role = isset($parts[0]) ? $parts[0] : '';
+
+                         // CHECK USER's ROLE is one of them
+                         if( $current_role !='' && in_array($current_role, $roles_names) )
+                         {
+                      @endphp
+                          <li class="nav-item MULTI-ROLED-USER">
+                            <a href="{{ route( $current_role . '.dashboard') }}"
+                            class="nav-link {{ Request::is( $current_role . '/dashboard*') ? 'active' : '' }}">
+                             <i class="ri-home-5-line"></i> <span>Dashboard</span></a>
+                          </li>
+                          <li class="nav-item MULTI-ROLED-USER">
+                             <a href="{{ route( $current_role . '.tickets.index') }}"
+                             class="nav-link {{ Request::is( $current_role . '/tickets*') ? 'active' : '' }}">
+                             <i class="ri-flag-2-line"></i> <span>Tickets</span></a>
+                          </li>
+                      @php
+                         }
+                      }
+                      @endphp
+                      <!-- for Multiple ROLES :: ENDS -->
                 @endif
             </ul>
         </div><!-- nav-group -->
@@ -80,12 +127,25 @@
             <div class="sidebar-footer-body">
                 <p>{{Auth::user()->roles[0]->name}}</p>
                 <h6><a class="text-capitalize" href="{{ url('/pages/profile') }}">{{Auth::user()->name}}</a></h6>
-                
+
             </div><!-- sidebar-footer-body -->
             <a id="sidebarFooterMenu" href="" class="dropdown-link"><i class="ri-arrow-down-s-line"></i></a>
         </div><!-- sidebar-footer-top -->
         <div class="sidebar-footer-menu">
             <nav class="nav">
+                @php
+                if($roleCount > 1)
+                {
+                    foreach($roles_names as $role)
+                    {
+                      if( strtolower($role) != strtolower($current_role) )
+                      {
+                        echo "<a href='/". strtolower($role) . "/dashboard'><i class='ri-edit-2-line'></i> View as " . ucwords($role) . "</a>";
+                      }
+
+                    }
+                }
+                @endphp
                 <a href=""><i class="ri-edit-2-line"></i> My Profile</a>
                 <a href=""><i class="ri-user-settings-line"></i> Settings</a>
                 <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
