b1f52a9520e22947181602623440cd33eca36903
Chandan Patra <chandan.patra@gmail.com>
2024-11-22 17:23:30 +0530

    Latest Fix

--- app/Http/Controllers/Ops/TicketController.php
+++ app/Http/Controllers/Ops/TicketController.php
@@ -429,7 +429,7 @@ class TicketController extends Controller
 						else // NOT MAIL to SELF
 						{
 							// SELL CASH case with SCREENSHOT
-							if( $this->ticket->payment_type == 1 && $ticket->security->amc->sellcashwosstmpl != null )
+							if( $ticket->payment_type == 1 && $ticket->security->amc->sellcashwosstmpl != null )
 							{
 								Mail::to($toEmail)->send(new TemplateBasedMailToAMC($ticket, 3, 0, 0));
 							}
